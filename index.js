08d3519d1abb6b053d80f13b54afebb28b146b25
onwaxcomm@gmail.com <onwaxcomm@gmail.com>
2022-07-01 22:33:10 +0200

    adding the newspaper by id endpoint

--- index.js
+++ index.js
@@ -53,4 +53,31 @@ app.get('/news',(req, res) => {
   res.json(articles)
 });
 
+app.get('/news/:newspaperId', async(req,res) => {
+   const newspaperId = req.params.newspaperId
+
+   const newspaperAddress = newspapers.filter(newspaper => newspaper.name === newspaperId)[0].address
+   const newspaperBase = newspapers.filter(newspaper => newspaper.name === newspaperId)[0].base
+
+   axios.get(newspaperAddress)
+     .then(response  => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const specificArticles = []
+
+        $('a:contains("climate")', html).each(function () {
+              const title = $(this).text()
+              const url = $(this).attr('href')
+
+              specificArticles.push({
+                title,
+                url: newspaperBase + url,
+                source: newspaperId
+
+              })
+        })
+        res.json(specificArticles)
+     }).catch(err => console.log(err))
+})
+
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
